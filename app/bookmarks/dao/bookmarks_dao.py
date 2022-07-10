6956466921fcb8364a4f0bf0f839c6fe2dcd96d6
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-07-10 17:48:08 +0500

    Del __pycache__

--- app/bookmarks/dao/bookmarks_dao.py
+++ app/bookmarks/dao/bookmarks_dao.py
@@ -12,14 +12,14 @@ class BookmarksDAO:
         return load_json(self.bookmarks_path)
 
     def add_bookmark(self, post: dict) -> None:
-        bookmarks = self.bookmarks()
+        bookmarks = self.all_bookmarks()
         added_bookmarks = {bookmark["pk"] for bookmark in bookmarks}
         if post["pk"] not in added_bookmarks:
             bookmarks.append(post)
             save_as_json(bookmarks, self.bookmarks_path)
 
     def del_bookmark(self, post: dict) -> None:
-        bookmarks = self.bookmarks()
+        bookmarks = self.all_bookmarks()
         for ind, bookmark in enumerate(bookmarks):
             if bookmark["pk"] == post["pk"]:
                 bookmarks.pop(ind)
