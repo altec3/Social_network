e6953255c7a3c84f1d5abc99e6b4d1ea6a98cc9d
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-07-12 17:57:51 +0500

    Made logging config in separate file

--- app/api/view.py
+++ app/api/view.py
@@ -2,13 +2,18 @@ from flask import Blueprint, jsonify
 import logging
 
 from app.main.view import main_dao
+from loggers_conf import create_logger
 
-logging.basicConfig(level=logging.INFO)
-logger_api = logging.getLogger("api")
-file_handler = logging.FileHandler("logs/api.log", encoding="utf-8")
-formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-file_handler.setFormatter(formatter)
-logger_api.addHandler(file_handler)
+create_logger()
+
+logger = logging.getLogger('basic')
+
+# logging.basicConfig(level=logging.INFO)
+# logger_api = logging.getLogger("api")
+# file_handler = logging.FileHandler("logs/api.log", encoding="utf-8")
+# formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+# file_handler.setFormatter(formatter)
+# logger_api.addHandler(file_handler)
 
 bp_get_json = Blueprint("bp_get_json", __name__)
 
@@ -17,7 +22,7 @@ bp_get_json = Blueprint("bp_get_json", __name__)
 def get_json_posts():
     posts = main_dao.posts_all()
     if posts:
-        logger_api.info(f"Запрос /api/posts/")
+        logger.info(f"Запрос /api/posts/")
         return jsonify(posts)
 
 
@@ -25,5 +30,5 @@ def get_json_posts():
 def get_json_post(post_id: int):
     post = main_dao.post_by_pk(post_id)
     if post:
-        logger_api.info(f"Запрос /api/posts/{post_id}")
+        logger.info(f"Запрос /api/posts/{post_id}")
         return jsonify(post)
